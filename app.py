f58f572f50c565f51e8aa80360f76346f2421adc
Djamel <djamelboulechfar@gmail.com>
2022-02-28 17:46:12 +0100

    Premier commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+import os
+
+app = Flask(__name__)
+api = Api(app)
+
+port = int(os.environ.get('PORT', 5000))
+
+
+class CalculeTempsParcours(Resource):
+    def get(self, distance, autonomie, temps_chargement, vitesse_moyenne):
+        nb_recharges_requises = distance // autonomie;
+        if distance % autonomie != 0:
+            nb_recharges_requises += 1
+        resultat = distance / vitesse_moyenne * 60 + nb_recharges_requises * temps_chargement
+        return {'tempsParcours': resultat}
+
+
+api.add_resource(CalculeTempsParcours, '/calculeTempsParcours/<int:distance>/<int:autonomie>/<int:temps_chargement>/<int:vitesse_moyenne>')
+
+if __name__ == '__main__':
+    # Local test
+    # app.run(host='localhost', port=port)
+    # Heroku
+    app.run(host='0.0.0.0', port=port)
